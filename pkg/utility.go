dc4597b13885e0edf136893408d13900586ec274
will.allen@venafi.com <will.allen@venafi.com>
2021-03-22 22:35:04 +0000

    very simple load/save

--- pkg/utility.go
+++ pkg/utility.go
@@ -11,11 +11,14 @@ type Utility struct {
 	displayname string
 	version     string
 	productcode string
+	conffile string
+
+	Configuration *Configuration
 }
 
 //NewUtility Creates a new applcation
-func NewUtility(productcode string, displayname string, version string) *Utility {
-	app := Utility{productcode: productcode, version: version, displayname: displayname}
+func NewUtility(productcode string, displayname string, version string,conffile string) *Utility {
+	app := Utility{productcode: productcode, version: version, displayname: displayname,conffile:conffile}
 	return &app
 }
 
@@ -33,3 +36,30 @@ func (app *Utility) GetDisplayname() string {
 func (app *Utility) GetProductCode() string {
 	return app.productcode
 }
+
+//LoadConf load utiliity configuration
+func (app *Utility) LoadConf() error {
+
+	if len(app.conffile) >0 {
+		conf, err := LoadConfig(app.conffile)
+		if err != nil {
+			return err
+		} else {
+			app.Configuration = conf
+		}
+	}
+
+	return nil
+}
+
+//SaveConf save utiliity configuration
+func (app *Utility) SaveConf() error {
+
+	// check we have a file path and data otherwise error
+	if len(app.conffile) > 0 &&  app.Configuration != nil{
+		return SaveConf(app.Configuration)s
+	}
+
+	return nil
+}
+
